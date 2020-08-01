d18b794b1c92d476a5f6989f6178016646d684b1
arthapatel97 <arthapatel97@gmail.com>
2020-08-01 14:03:05 -0500

    updated parser code

--- parser.py
+++ parser.py
@@ -1,13 +1,16 @@
 import mysql.connector
 import requests
 from bs4 import BeautifulSoup
+import random
+import warnings
+warnings.filterwarnings("ignore")
+
+nums = random.sample(range(1000000,3000000), 200)
 
 mydb = mysql.connector.connect(user='root', password='password',
                               host='localhost',
                               database='Case_record')
 
-
-entry_num = 40
 case_id = 50
 person_id = 50
 person_id2 = 51
@@ -15,8 +18,8 @@ people_of_interest_id = 1
 location_id = 100
 
 
-for i in range(100):
-    URL = "https://www.fjc.gov/research/idb/interactive/detail/IDB-civil-since-1988/" + str(entry_num) + "?width=650&height=800"
+for j in range(len(nums)):
+    URL = "https://www.fjc.gov/research/idb/interactive/detail/IDB-civil-since-1988/" + str(nums[j]) + "?width=650&height=800"
     page = requests.get(URL, verify=False)
 
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -32,6 +35,7 @@ for i in range(100):
 
     rows = first.findAll("tr")
     arr = []
+    print(j)
     for row in rows:
         dataobj = row.find("td")
 
@@ -41,7 +45,7 @@ for i in range(100):
     plaintiff = arr[21]
     defendant = arr[22]
     circuit = int(arr[0])
-    district = int(arr[1])
+    district = arr[1]
     office = int(arr[2])
     docket_num = int(arr[3])
     filing_date = arr[5]
@@ -70,12 +74,11 @@ for i in range(100):
     mycursor.execute(sql, val)
 
 
-    sql = "INSERT IGNORE INTO Cases (docket_number, location_id, case_name, filing_date, termination_date, disposition, nature, judgement) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-    val = (docket_num, location_id, case_name, filing_date, termination_date, disposition, natureofjudgement, judgement)
+    sql = "INSERT IGNORE INTO Cases (docket_number, location_id, case_name, filing_date, termination_date, disposition, nature, judgement, people_of_interest_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (docket_num, location_id, case_name, filing_date, termination_date, disposition, natureofjudgement, judgement,people_of_interest_id)
     mycursor.execute(sql, val)
 
     mydb.commit()
-    entry_num += 1
     person_id += 2
     person_id2 += 2
     location_id += 1
